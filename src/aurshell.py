6b8e4dae5c80ec4fbab3f9e2b45821d36b810fe4
Piotr Husiatyński <phusiatynski@gmail.com>
2008-03-31 21:15:47 +0200

    aurshell can run in signle command mode now. If arguments were given, aurshell will execute the command and quit.

--- src/aurshell.py
+++ src/aurshell.py
@@ -20,23 +20,31 @@
 
 
 
-import signal
 import sys
 import os
 
 from shell import shell
 import conf
 
+
 def signal_handler(sig, frame):
     pass
 
+
 if __name__ == '__main__':
-    #signal.signal(signal.SIGINT, signal_handler)
-    try:
-        aursh = shell.Shell(conf)
-        aursh.cmdloop()
-    except KeyboardInterrupt:
-        # TODO [ 23:18 - 15.03.2008 ] 
-        # C^c shouldn't quit shell
-        pass
+    # run in single command mode
+    # only if arguments were given
+    if len(sys.argv) > 1:
+        aurshell = shell.Shell(conf)
+        aurshell.default(" ".join(sys.argv[1:]))
+        sys.exit()
+    else:
+    # run shell
+        try:
+            aursh = shell.Shell(conf)
+            aursh.cmdloop()
+        except KeyboardInterrupt:
+            # TODO [ 23:18 - 15.03.2008 ] 
+            # C^c shouldn't quit shell
+            pass
     
